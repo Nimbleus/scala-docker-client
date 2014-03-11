f5a781e0c4d2da5e7efaf801260713774bb0ab79
cstewart <cstewart@nimbleus.com>
2014-03-11 11:13:45 -0400

    fixed port inspection

--- src/main/scala/com/nimbleus/docker/client/model/DockerProtocol.scala
+++ src/main/scala/com/nimbleus/docker/client/model/DockerProtocol.scala
@@ -13,6 +13,7 @@ import spray.json.DefaultJsonProtocol
 object DockerProtocol extends DefaultJsonProtocol {
   implicit val versionProtocol = jsonFormat3(Version)
   implicit val portProtocol = jsonFormat3(Port)
+  implicit val port2Protocol = jsonFormat1(Port2)
   implicit val containerProtocol = jsonFormat9(Container.apply)
   implicit val imageProtocol = jsonFormat5(Image)
   implicit val containerProcessProtocol = jsonFormat2(ContainerProcess)
@@ -22,7 +23,7 @@ object DockerProtocol extends DefaultJsonProtocol {
   implicit val createContainerConfigProtocol = jsonFormat15(ContainerConfig)
   implicit val createContainerLxcConfProtocol = jsonFormat2(ContainerLxcConf)
   implicit val createContainerStateProtocol = jsonFormat6(ContainerState)
-  implicit val createContainerNetworkSettingsProtocol = jsonFormat5(ContainerNetworkSettings)
+  implicit val createContainerNetworkSettingsProtocol = jsonFormat6(ContainerNetworkSettings)
   implicit val createContainerHostConfigProtocol = jsonFormat7(ContainerHostConfig)
   implicit val createInspectContainerResponseProtocol = jsonFormat15(InspectContainerResponse.apply)
 }
