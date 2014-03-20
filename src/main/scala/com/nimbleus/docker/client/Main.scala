409f9dd414546e3bb6523cc5ab3f7cdb569b5dd3
cstewart <cstewart@nimbleus.com>
2014-03-20 19:30:13 -0400

    fixed list container

--- src/main/scala/com/nimbleus/docker/client/Main.scala
+++ src/main/scala/com/nimbleus/docker/client/Main.scala
@@ -19,18 +19,10 @@ object Main extends App {
   val serverUrl : String = "http://localhost:4243"
 
 /*
-  val versionResponse = DockerClient.getVersion(serverUrl)
-  versionResponse onComplete {
-    case Success(result: Version) => {
-      println("version => " + result.toString)
-    }
-    case Failure(error) =>
-      println(error, "Couldn't retrieve version")
-  }
-*/
+  val p1: ContainerParam = ContainerParamAll(true)
+  //val p2: ContainerParam = ContainerParamLimit()
 
-/*
-  val containerResponse = DockerClient.listContainers(serverUrl)
+  val containerResponse = DockerClient.listContainers(serverUrl, p1)
   containerResponse onComplete {
     case Success(result: List[Container]) => {
       println("containers => " + result.toString)
@@ -57,6 +49,44 @@ object Main extends App {
     }
   }
 */
+/*
+  val versionResponse = DockerClient.getVersion(serverUrl)
+  versionResponse onComplete {
+    case Success(result: Version) => {
+      println("version => " + result.toString)
+    }
+    case Failure(error) =>
+      println(error, "Couldn't retrieve version")
+  }
+*/
+
+  val p1: ContainerParam = ContainerParamAll(true)
+  val containerResponse = DockerClient.listContainers(serverUrl, p1)
+  containerResponse onComplete {
+    case Success(result: List[Container]) => {
+      println("containers => " + result.toString)
+      for(c <- result){
+        val containerProcessorResponse = DockerClient.getContainerProcesses(serverUrl, c.Id)
+        containerProcessorResponse onComplete {
+          case Success(result: ContainerProcess) => {
+            println("container processes => " + result.toString)
+          }
+          case Failure(error: UnsuccessfulResponseException) => {
+            println(ContainerProcessHelper.getErrorReason(error.response.status.intValue, error.response.message.toString))
+          }
+          case Failure(e) => {
+            println(e, "Couldn't list container processes")
+          }
+        }
+      }
+    }
+    case Failure(error: UnsuccessfulResponseException) => {
+      println(Container.getErrorReason(error.response.status.intValue, error.response.message.toString))
+    }
+    case Failure(e) =>{
+      println(e, "Couldn't list containers")
+    }
+  }
 
   //val force: Boolean = false
   //println(force.toString.toLowerCase)
